b20ac09b043b0f0b86a77c563db4f551249aeb14
kannanATexcelforte <kannan@excelforte.com>
2024-04-18 12:06:27 +0530

    18.04.2024-cfatno
    completed

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const Person = function(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // never do this
+    // this.calcAge = function() {
+    //     console.log(2037 - birthYear);
+    // }
+}
+
+const jonas = new Person('Jonas',  1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda',  2017);
+const jack = new Person('Jack',  1975);
+console.log(matilda, jack);
+
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
