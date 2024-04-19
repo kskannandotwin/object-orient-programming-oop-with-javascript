0bb561eb90ca8bc985ba8e4f0ae3a76f4b5b7e15
kannanATexcelforte <kannan@excelforte.com>
2024-04-19 15:11:54 +0530

    19.04.2024-cc#1
    completed

--- script.js
+++ script.js
@@ -1,72 +1,40 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property.
+The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the
+new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed
+to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times
+on each of them.
 
-    // never do this
-    // this.calcAge = function() {
-    //     console.log(2037 - birthYear);
-    // }
-}
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+GOOD LUCK
+*/
 
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
-console.log(matilda, jack);
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
 
-const jay = 'Jay';
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
 
-console.log(jonas instanceof Person);
-console.log(jay instanceof Person);
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
 
-// prototypes
-console.log(Person.prototype);
-Person.prototype.calcAge = function () {
-    console.log(2037 - this.birthYear);
-}
-
-jonas.calcAge();
-matilda.calcAge();
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-// .prototypeOfLinkedObjects
-
-Person.prototype.species = 'Homo sapiens';
-console.log(jonas.species, matilda.species);
-
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
-
-console.log(jonas.__proto__);
-// Object.prototype (top of prototype chain)
-console.log(jonas.__proto__.__proto__);
-console.log(jonas.__proto__.__proto__.__proto__);
-
-console.dir(Person.prototype.constructor);
-
-const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-
-console.log(arr.__proto__.__proto__);
-
-Array.prototype.unique = function() {
-    return [...new Set(this)]
-}
-console.log(arr.unique());
-
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1);
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
