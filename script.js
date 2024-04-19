bdfdd1d2054d9ea992e91fba4dceb8ac671cc3b3
kannanATexcelforte <kannan@excelforte.com>
2024-04-19 16:27:21 +0530

    19.04.2024-ec
    completed

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 'use strict';
 
-/* 
-1. Use a constructor function to implement a Car. A car has a make and a speed property.
-The speed property is the current speed of the car in km/h;
-2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the
-new speed to the console;
-3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed
-to the console;
-4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times
-on each of them.
+// class expression
+// const PersonCl = class { }
 
-DATA CAR 1: 'BMW' going at 120 km/h
-DATA CAR 2: 'Mercedes' going at 95 km/h
+// class declaration
+class PersonCl {
+    constructor(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
 
-GOOD LUCK
-*/
+    // methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
 
-const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed;
+    greet() {
+        console.log(`Hey ${this.firstName}`);
+    }
 };
 
-Car.prototype.accelerate = function () {
-    this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-};
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
 
-Car.prototype.brake = function () {
-    this.speed -= 5;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-};
+console.log(jessica.__proto__ === PersonCl.prototype);
 
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('Mercedes', 95);
+// PersonCl.prototype.greet = function() {
+//     console.log(`Hey ${this.firstName}`);
+// }
+jessica.greet();
 
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
-bmw.accelerate();
+// 1. classes are NOT hoisted
+// 2. classes are first class citizens
+// 3. classes are executed in strict mode
