32fb4277f137f12bba39096f5edb7e2544bb387f
kskannandotwin <kskannan.win@gmail.com>
2024-04-21 18:36:25 +0530

    21.04.2024-fix
    completed

--- script.js
+++ script.js
@@ -1,24 +1,95 @@
 'use strict';
 
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // never do this
+  // this.calcAge = function() {
+  //     console.log(2037 - birthYear);
+  // }
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
+
+// prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+
 // class expression
 // const PersonCl = class { }
 
 // class declaration
 class PersonCl {
-    constructor(firstName, birthYear) {
-        this.firstName = firstName;
-        this.birthYear = birthYear;
-    }
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
 
-    // methods will be added to .prototype property
-    calcAge() {
-        console.log(2037 - this.birthYear);
-    }
+  // methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
 
-    greet() {
-        console.log(`Hey ${this.firstName}`);
-    }
-};
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
 
 const jessica = new PersonCl('Jessica', 1996);
 console.log(jessica);
