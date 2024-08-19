7300c055c0d68fb8d7e3e3464f9f080e0ed262e5
kskannandotwin <kskannan.win@gmail.com>
2024-08-19 19:31:26 +0530

    19.08.2024-ibccf
    completed

--- script.js
+++ script.js
@@ -3,180 +3,35 @@
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
-
-  // never do this
-  // this.calcAge = function() {
-  //     console.log(2037 - birthYear);
-  // }
 };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
-
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
-
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
-console.log(matilda, jack);
-
-const jay = 'Jay';
-
-console.log(jonas instanceof Person);
-console.log(jay instanceof Person);
-
-Person.hey = function() {
-  console.log('Hey there!');
-  console.log(this);
-};
-
-Person.hey();
-
-// prototypes
-console.log(Person.prototype);
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
 
-jonas.calcAge();
-matilda.calcAge();
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-// .prototypeOfLinkedObjects
-
-Person.prototype.species = 'Homo sapiens';
-console.log(jonas.species, matilda.species);
-
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
-
-console.log(jonas.__proto__);
-// Object.prototype (top of prototype chain)
-console.log(jonas.__proto__.__proto__);
-console.log(jonas.__proto__.__proto__.__proto__);
-
-console.dir(Person.prototype.constructor);
-
-const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-
-console.log(arr.__proto__.__proto__);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
-console.log(arr.unique());
-
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1);
-
-// class expression
-// const PersonCl = class { }
-
-// class declaration
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
-
-  // instance methods
-  // methods will be added to .prototype property
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
-
-  greet() {
-    console.log(`Hey ${this.firstName}`);
-  }
-
-  get age() {
-    return 2037 - this.birthYear;
-  }
-
-  //   set a property that already exists
-  set fullName(name) {
-    console.log(name);
-    if (name.includes(' ')) this._fullName = name;
-    else alert(`${name} is not a full name.`);
-  }
-
-  get fullName() {
-    return this._fullName;
-  }
-
-  // static method
-  static hey() {
-    console.log('Hey there!');
-    console.log(this);
-  }
-}
-
-const jessica = new PersonCl('Jessica Davis', 1996);
-console.log(jessica);
-jessica.calcAge();
-console.log(jessica.age);
-
-console.log(jessica.__proto__ === PersonCl.prototype);
-
-// PersonCl.prototype.greet = function() {
-//     console.log(`Hey ${this.firstName}`);
-// }
-jessica.greet();
-
-// 1. classes are NOT hoisted
-// 2. classes are first class citizens
-// 3. classes are executed in strict mode
-
-const walter = new PersonCl('Walter White', 1965);
-PersonCl.hey();
-
-const account = {
-  owner: 'Jonas',
-  movements: [200, 530, 120, 300],
-
-  get latest() {
-    return this.movements.slice(-1).pop();
-  },
-
-  set latest(mov) {
-    this.movements.push(mov);
-  },
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
 };
 
-console.log(account.latest);
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
 
-account.latest = 50;
-console.log(account.movements);
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
 
+const mike = new Student('Mike', 2020, 'Computer Science');
+console.log(mike);
+mike.introduce();
+mike.calcAge();
 
-const PersonProto = {
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  },
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
 
-  init(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  }
-}
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
 
-const steven = Object.create(PersonProto);
-console.log(steven);
-steven.name = 'Steven';
-steven.birthYear = 2002;
-steven.calcAge();
-
-console.log(steven.__proto__ === PersonProto);
-
-const sarah = Object.create(PersonProto);
-sarah.init('Sarah', 1979);
-sarah.calcAge();
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
