3b9b942b0230bc1f8ae1716c24830b0a65d83382
kskannandotwin <kskannan.win@gmail.com>
2024-10-08 06:20:51 +0530

    08.10.2024-cm
    completed

--- script.js
+++ script.js
@@ -34,10 +34,12 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+    return this;
   }
 
   requestLoan(val) {
@@ -45,6 +47,7 @@ class Account {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved.`);
+      return this;
     }
   }
 
@@ -72,9 +75,12 @@ acc1.requestLoan(1000);
 
 console.log(acc1.getMovements());
 console.log(acc1);
+Account.helper();
 
 // console.log(acc1.#movements);
 // console.log(acc1.#pin);
 // console.log(acc1.#aprovedLoan(100));
 
-Account.helper();
+// chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
